6b3dff19b9681c8a28233c0195185fb707d235d4
Dhruva966 <69322555+Dhruva966@users.noreply.github.com>
2023-09-03 10:43:45 -0700

    Update mixing(3).cpp

--- mixing(3).cpp
+++ mixing(3).cpp
@@ -10,71 +10,65 @@ using namespace std;
 #include <algorithm>
 #include <cmath>
 
-
 int main() {
-	
-//ifstream fin("mixmilk.in");
+    // Input streams for reading and output streams for writing are commented out.
+    // ifstream fin("mixmilk.in");
+    // ofstream fout("mixmilk.out");
 
-//ofstream fout("mixmilk.out");
+    // Create a vector of pairs to represent the three buckets of milk.
+    vector<pair<long long, long long>> milks(3);
 
-
-
-	    vector<pair<long long, long long>> milks(3); // Creating a vector to store the pairs
-
-  for (int i = 0; i < 3; i++) {
+    // Read the initial milk capacity and amount of milk in each bucket.
+    for (int i = 0; i < 3; i++) {
         long long a, b;
         cin >> a >> b;
         milks[i] = make_pair(a, b);
     }
 
+    // Perform 100 pour operations.
+    for (int e = 0; e < 2; e++) {
+        for (int u = 0; u < 3; u++) {
+            if (u == 2) {
+                // Pour from the third bucket to the first bucket.
+                milks[0].second += milks[2].second;
+                milks[2].second = 0;
 
-for(int e = 0; e < 2; e++){
-		for(int u = 0; u < 3; u++){
-			
-			if(u == 2){
+                // Check if the first bucket has overflowed.
+                if (milks[0].second > milks[0].first) {
+                    milks[2].second += (milks[0].second - milks[0].first);
+                    milks[0].second = milks[0].first;
+                }
+            } else {
+                // Pour from one bucket to the next.
+                milks[u + 1].second += milks[u].second;
+                milks[u].second = 0;
 
-				milks[0].second += milks[2].second;
-				milks[2].second = 0;
+                // Check if the next bucket has overflowed.
+                if (milks[u + 1].second > milks[u + 1].first) {
+                    milks[u].second += (milks[u + 1].second - milks[u + 1].first);
+                    milks[u + 1].second = milks[u + 1].first;
+                }
+            }
+        }
+    }
 
-				if(milks[0].second > milks[0].first){
+    // Pour the remaining milk from bucket 1 to bucket 2.
+    milks[1].second += milks[0].second;
+    milks[0].second = 0;
 
-				milks[2].second += (milks[0].second - milks[0].first);
+    // Check if bucket 2 has overflowed.
+    if (milks[1].second > milks[1].first) {
+        milks[0].second += (milks[1].second - milks[1].first);
+        milks[1].second = milks[1].first;
+    }
 
-				milks[0].second = milks[0].first;
-				
-				}
+    // Output the final amount of milk in each bucket.
+    cout << milks[0].second << "\n" << milks[1].second << "\n" << milks[2].second;
 
-			} else{
-			
-			milks[u+1].second += milks[u].second;
-			milks[u].second = 0;
+    // Close the input and output streams if they were opened.
+    // fin.close();
+    // fout.close();
 
-			if(milks[u+1].second > milks[u+1].first){
-
-				milks[u].second += (milks[u+1].second - milks[u+1].first);
-				milks[u+1].second = milks[u+1].first;
-			} 
-
-		}
-
-		}
-		
+    // End of the program.
+    return 0;
 }
-
-	milks[1].second += milks[0].second;
-			milks[0].second = 0;
-
-			if(milks[1].second > milks[1].first){
-
-				milks[0].second += (milks[1].second - milks[1].first);
-				milks[1].second = milks[1].first;
-			}
-
-
-				cout << milks[0].second << "\n" << milks[1].second << "\n" << milks[2].second;
-
-
-
-
-}
-
